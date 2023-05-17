5828c8120c9e4e841484c1194df0961f76034cdc
Olivia <oliviaclarkeedwards@gmail.com>
2023-05-18 11:11:33 +1200

    add problem

---
+++ day2-operators.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'solve' function below.
+#
+# The function accepts following parameters:
+#  1. DOUBLE meal_cost
+#  2. INTEGER tip_percent
+#  3. INTEGER tax_percent
+#
+
+def solve(meal_cost, tip_percent, tax_percent):
+    # Write your code here
