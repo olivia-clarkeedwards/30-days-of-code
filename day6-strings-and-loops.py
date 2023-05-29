79d7a2b6677070315a61f6661cde5a76a136ea28
Olivia <oliviaclarkeedwards@gmail.com>
2023-05-30 11:57:08 +1200

    seperate and print odd and even indexed characters

---
+++ day6-strings-and-loops.py
@@ -0,0 +1,24 @@
+
+
+def printEvenAndOddChars(s):
+    even_string = ""
+    odd_string = ""
+    
+    for index in range(len(s)):
+        if index % 2 == 0:
+            even_string += s[index]
+        else:
+            odd_string += s[index]
+
+    print(f"{even_string} {odd_string}")
+    
+
+def processInput():
+    numInputs = int(input())
+
+    for _ in range(numInputs):
+        str = input()
+
+        printEvenAndOddChars(str)
+
+processInput()
