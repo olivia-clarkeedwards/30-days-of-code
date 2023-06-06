c6db356da896562cc565d4d0f07368497fa6465c
Olivia <oliviaclarkeedwards@gmail.com>
2023-06-06 16:48:41 +1200

    print reversed array

---
+++ day7-reverse-array.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+
+def printReversedArray(arr):
+    
+    for i in range(len(arr) - 1, -1, -1):
+        print(arr[i], end=" ")
+    
+
+
+n = int(input().strip())
+
+arr = list(map(int, input().rstrip().split()))
+
+printReversedArray(arr)
